009182c372f3ef8549162abe0eb98d90b85cd45d
Vinayak-R-Lamani <rajulamani9888@gmail.com>
2024-11-19 12:20:03 +0530

    the encapsulation

---
+++ encapsulation.py
@@ -0,0 +1,21 @@
+class Person:
+    def __init__(self , name: str , age: int):
+        self._name = name
+        self._age = age
+        
+    def get_name(self):
+        return self._name
+    
+    def get_age(self):
+        return self._age
+
+        
+class Main:
+    @staticmethod
+    def main():
+        person = Person('name' ,12)
+        print(person.get_name())
+        print(person.get_age())
+        
+if __name__== "__main__":
+    Main.main()
